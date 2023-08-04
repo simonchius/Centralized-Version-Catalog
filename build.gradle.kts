9022ca39e844d884f064446c554c748fdd14ec24
simon <simon@anywhere.co>
2023-08-04 15:38:48 +0530

    Fixed issue on lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,19 @@ plugins {
     `maven-publish`
 }
 
-/* Use this code to change the version catalog file location. By default it refers from gradle/libs.versions.toml
 catalog {
     versionCatalog {
         from(files("gradle/libs.versions.toml"))
 
     }
 }
-*/
 
 publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = "com.simonchius.android"
             artifactId = "version.catalog"
-            version = "1.0.2"
+            version = "1.0.3"
             from(components["versionCatalog"])
         }
     }
